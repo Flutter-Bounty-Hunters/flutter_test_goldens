e35c5d0ebb0cbf24bf33090199ec576671ebed26
Matt Carroll <matthew-carroll@users.noreply.github.com>
2025-06-17 12:13:00 -0700

    Improve golden API ergonomics, reduce need for pumper where possible (Resolves #41) (#42)
    * Added @isGoldenScene tag to test runners
    * Added support for different platforms per item in a Gallery.
    * Screenshots now capture the whole widget tree and then extracts a desired region, so that the screenshots capture pixels in the app overlay.
    * Added per golden size control

--- lib/src/goldens/golden_camera.dart
+++ lib/src/goldens/golden_camera.dart
@@ -16,6 +16,11 @@ class GoldenCamera {
   /// along with its [description].
   ///
   /// {@macro golden_image_bounds_default_finder}
+  ///
+  /// The photo captures a screenshot of the entire widget tree, and then extracts the pixels
+  /// within the [finder] region. This requires moving more pixel data, but this is done so
+  /// that the photo captures widgets that sit in the app overlay, such as the mobile drag
+  /// handles, magnifier, or popover toolbar for a text field.
   Future<void> takePhoto(String description, [Finder? finder]) async {
     finder = finder ?? find.byType(GoldenImageBounds);
 
@@ -23,35 +28,23 @@ class GoldenCamera {
 
     final renderObject = finder.evaluate().first.findRenderObject();
     late final Image photo;
-    if (renderObject!.isRepaintBoundary) {
-      // The render object that we want to screenshot is already a repaint boundary,
-      // so we can directly request an image from it.
-      final repaintBoundary = finder.evaluate().first.renderObject! as RenderRepaintBoundary;
-      photo = await repaintBoundary.toImage(pixelRatio: 1.0);
-    } else {
-      // The render object that we want to screenshot is NOT a repaint boundary, so we need
-      // to screenshot the entire UI and then extract the region belonging to this widget.
-      if (renderObject is! RenderBox) {
-        throw Exception(
-          "Can't take screenshot because the root of the widget tree isn't a RenderBox. It's a ${renderObject.runtimeType}",
-        );
-      }
-
-      // TODO: Try the following approach. It probably doesn't work because we're
-      //       using a TestRecordingPaintingContext with a non-test version of Canvas.
-      //       But maybe it will work out.
-      final pictureRecorder = PictureRecorder();
-      final canvas = Canvas(pictureRecorder);
-      final screenSize = renderObject.size;
-
-      final paintingContext = TestRecordingPaintingContext(canvas);
-      renderObject.paint(paintingContext, Offset.zero);
-
-      photo = await pictureRecorder.endRecording().toImage(
-            screenSize.width.round(),
-            screenSize.height.round(),
-          );
+    if (renderObject is! RenderBox) {
+      throw Exception(
+        "Can't take screenshot because the root of the widget tree isn't a RenderBox. It's a ${renderObject.runtimeType}",
+      );
     }
+
+    final pictureRecorder = PictureRecorder();
+    final canvas = Canvas(pictureRecorder);
+    final screenSize = renderObject.size;
+
+    final paintingContext = TestRecordingPaintingContext(canvas);
+    renderObject.paint(paintingContext, Offset.zero);
+
+    photo = await pictureRecorder.endRecording().toImage(
+          screenSize.width.round(),
+          screenSize.height.round(),
+        );
 
     _photos.add(
       GoldenPhoto(description, photo),
---
+++ lib/src/test_builders.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
+// SingleShotTestBuilder buildSingleShotTestOnMac(String testName) {
+//   return SingleShotTestBuilder(testName, TargetPlatform.macOS);
+// }
+//
+// class SingleShotTestBuilder {
+//   const SingleShotTestBuilder(this.testName, this.platform);
+//
+//   final String testName;
+//   final TargetPlatform platform;
+//
+//   /// The directory where the golden scene file will be saved.
+//   final Directory? directory;
+//
+//   /// The file name for the golden scene file, which will be saved in [_directory].
+//   final String? fileName;
+// }
