134bc96d95d7a55e6ab26ca5366d71497a875440
Matt Carroll <matthew-carroll@users.noreply.github.com>
2026-04-26 17:25:47 -0700

    [Feature] - Let users pass a custom FlutterCamera to gallery runner (#81)

---
+++ test_goldens/scene_types/gallery/gallery_custom_camera_test.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test_goldens/flutter_test_goldens.dart';
+
+void main() {
+  group("Scene types > gallery >", () {
+    testGoldenScene(
+      "uses custom camera instance",
+      (tester) async {
+        final camera = _CustomCamera();
+        final itemWidget = FlutterLogo();
+
+        await Gallery(
+          "Custom Camera",
+          fileName: "gallery_custom_camera",
+          layout: ColumnSceneLayout(),
+        )
+            .itemFromWidget(
+              id: "1",
+              description: "first",
+              widget: itemWidget,
+            )
+            .itemFromWidget(
+              id: "2",
+              description: "second",
+              widget: itemWidget,
+            )
+            .run(tester, camera: camera);
+
+        expect(camera._photoCount, equals(2));
+      },
+    );
+  });
+}
+
+class _CustomCamera extends FlutterCamera {
+  int _photoCount = 0;
+
+  @override
+  Future<void> takePhoto(String id, [Finder? finder]) async {
+    _photoCount++;
+    await super.takePhoto(id, finder);
+  }
+}
